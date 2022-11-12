ffdef84d5e015b607997485fe2c749b4f21e60c4
Tommy Skeffington <admin@skeffingtons.uk>
2022-11-12 15:47:09 +0000

    updated 2017 q1 to be more concise

--- 2017/q1-2017.py
+++ 2017/q1-2017.py
@@ -2,12 +2,9 @@ COLOURS = ["R", "G", "B"]
 
 def find_missing(a: str, b: str) -> str:
     if a == b: return a
-    lst = COLOURS[:]
-    lst.remove(a)
-    lst.remove(b)
-    return lst[0]
+    return "".join(["" if a == c or b == c else c for c in COLOURS])
 
-def find_square(row) -> str:
+def find_last(row) -> str:
     for _ in range(len(row) - 1): row = [find_missing(c, row[i + 1]) for i, c in enumerate(row[:-1])]
     return row[0]
 
