4fbd19a041d1aeaa06897714ae1f9a15224e82ce
Tommy Skeffington <admin@skeffingtons.uk>
2022-12-03 00:25:24 +0000

    Update q3-2022.py

--- 2022/q3-2022.py
+++ 2022/q3-2022.py
@@ -1,41 +1,28 @@
-ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-
-def product(lst: list, f=1) -> int:
-    for n in lst:
-        f *= n
-    return f
-
+import string, math
 
 def find_preference(arrangement: str, index: int) -> str:
-    index -= 1
-    preferences = []
-    length = len(arrangement)
-    layout = [None] * length
-
-    for i in range(length):
-        letter, permutations = ALPHABET[i].lower(), []
-        position = arrangement.index(letter)
-
+    alphabet, layout, preferences = string.ascii_lowercase, [None] * len(arrangement), []
+    for c in alphabet[:len(arrangement)]:
+        position, permutations = arrangement.index(c), []
         for j in range(position):
-            if layout[j] == None:
-                permutations.clear()
-            else:
+            if layout[j]:
                 permutations.append(j)
+            else:
+                permutations.clear()
 
         permutations.append(position)
-        layout[position] = letter
+        layout[position] = c
         preferences.append(sorted(permutations))
-
+  
     lengths, permutation = list(map(len, preferences)), []
-    for i in range(length):
-        multiplied = product(lengths[i + 1:])
+    for i, selection in enumerate(preferences):
+        multiplied = math.prod(lengths[i + 1:])
         n = index // multiplied
-        permutation.append(preferences[i][n])
+        permutation.append(selection[n])
         index -= n * multiplied
 
-    return "".join([ALPHABET[i] for i in permutation])
+    return "".join([alphabet[i].upper() for i in permutation])
 
 
 [combination, index] = input().split()
-print(find_preference(combination, int(index)))
+print(find_preference(combination, int(index) - 1))
